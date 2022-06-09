cfeb1e80caa0e59a2769b55ff49dbc24e8d0ab03
Jake <jake.ord345@gmail.com>
2022-06-09 19:03:35 +0100

    added client mutations and setup tailwind + icons

--- src/backend/routers/project-router.ts
+++ src/backend/routers/project-router.ts
@@ -2,6 +2,10 @@ import * as trpc from "@trpc/server";
 import { z } from "zod";
 import { prisma } from "../db";
 
+const ProjectEnum = z
+  .enum(["Not Started", "In Progress", "Completed"])
+  .default("Not Started");
+
 export const projectRouter = trpc
   .router()
   .query("findAll", {
@@ -19,6 +23,54 @@ export const projectRouter = trpc
       return await prisma.project.findFirst({
         where: { id: input.id },
         include: { client: true }
+      });
+    }
+  })
+  .mutation("create", {
+    input: z.object({
+      name: z.string(),
+      description: z.string(),
+      status: ProjectEnum,
+      client: z.object({
+        connect: z.object({ id: z.string() })
+      })
+    }),
+    async resolve({ input }) {
+      return await prisma.project.create({ data: input });
+    }
+  })
+  .mutation("delete", {
+    input: z.object({
+      id: z.string()
+    }),
+    async resolve({ input }) {
+      return await prisma.project.delete({
+        where: { id: input.id }
+      });
+    }
+  })
+  .mutation("update", {
+    input: z.object({
+      id: z.string(),
+      name: z.string().optional(),
+      description: z.string().optional(),
+      status: ProjectEnum.optional(),
+      client: z
+        .object({
+          connect: z.object({ id: z.string() })
+        })
+        .optional()
+    }),
+    async resolve({ input }) {
+      return await prisma.project.upsert({
+        where: { id: input.id },
+        update: input,
+        create: {
+          name: input.name!,
+          description: input.description!,
+          status: input.status!,
+          client: input.client!
+        }
       });
     }
   });
