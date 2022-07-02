0b10070a7b0d6392e860a74b4ab21f4fd294425a
Jake <jake.ord345@gmail.com>
2022-07-02 17:47:09 +0100

    debug vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,6 +31,9 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
+
+    console.log(process.env.VERCEL_URL);
+
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
